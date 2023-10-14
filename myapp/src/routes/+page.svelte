0d03f26df7ddc1936d5ed0d28395762f27e0f36d
Abrar2003 <spartanabrar@gmail.com>
2023-10-14 23:10:08 +0530

    backend - api - register / done / MVC

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script>
+  import { onMount } from "svelte";
   import CareerPossibilities from "./CareerPossibilities/CareerPossibilities.svelte";
   import CertificateNsdc from "./CertificateByNSDC/CertificateNSDC.svelte";
   import Footer from "./Footer/Footer.svelte";
   import MasaiNews from "./MasaiNews/MasaiNews.svelte";
 import Navbar from "./Navbar.svelte";
 
+onMount(() => {
+	fetch("http://localhost:5000/").then((res) => res.json).then(res => console.log(res));
+})
 </script>
 
 <svelte:head>
