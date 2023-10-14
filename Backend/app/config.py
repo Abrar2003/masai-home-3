d03a8abaf1b90f546585f1ef2b4869f9d7067e76
Abrar2003 <spartanabrar@gmail.com>
2023-10-14 23:19:54 +0530

    error resolved in config file

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,3 +1,3 @@
 class Config:
     SECRET_KEY = 'your_secret_key'  #TO-DO : USE ENVIRONMENT VARIABLES
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Man)0#Riz@localhost/flaskdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost/flaskdb'
